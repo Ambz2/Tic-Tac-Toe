d0c56bd604498ae5fc65698009e4560ea8526293
ameen <ameenahmed1998@gmail.com>
2021-11-08 16:55:17 +0000

    began work on gameboard module

---
+++ index.js
@@ -0,0 +1,18 @@
+const gameBoard = (function() {
+    'use strict';
+    const board = [
+        
+    ]
+
+    const cells = document.querySelectorAll('.cell')
+
+    return {
+        updateDisplay: function() {
+
+        }
+    }
+
+})()
+    
+    
+gameBoard.updateDisplay()
